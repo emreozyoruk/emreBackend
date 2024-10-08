c9d50d706ea0cb5affd7fed811ffd1a4990d05ee
Seras <72655616+emreozyoruk@users.noreply.github.com>
2024-10-08 21:02:46 +0300

    emreBackend

---
+++ emreBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace emreBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
